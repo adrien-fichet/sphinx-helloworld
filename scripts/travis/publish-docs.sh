ca5866b01b89eb0e61732a29faa85930628df506
sniksnp <sniksnp@gmail.com>
2014-04-16 21:14:30 +0200

    List files after sphinx build

--- scripts/travis/publish-docs.sh
+++ scripts/travis/publish-docs.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-git config credential.helper "store --file=.git/credentials"
-echo "https://${TOKEN}:@github.com" > .git/credentials
-git config user.name "sniksnp"
-git add doc/html
-git commit -m "Automatic push from Travis"
-git push
+ls -l
